8657ff18c85d3725171045b1230a00abdae2172f
Valery Piashchynski <piashchynski.valery@gmail.com>
2023-10-05 12:42:31 +0200

    chore: correct PHP init
    Signed-off-by: Valery Piashchynski <piashchynski.valery@gmail.com>

--- tests/php_test_files/create_3_services.php
+++ tests/php_test_files/create_3_services.php
@@ -4,7 +4,7 @@ use Spiral\RoadRunner\Services\Exception\ServiceException;
 use Spiral\Goridge\RPC\RPC;
 
 ini_set('display_errors', 'stderr');
-require dirname(__DIR__) . "/vendor/autoload.php";
+require __DIR__ . "/vendor/autoload.php";
 
 $rpc = RPC::create('tcp://127.0.0.1:6001');
 $manager = new Manager($rpc);
