3d48fb4217fa5b4976fc43236d3279a068be5a05
Valery Piashchynski <piashchynski.valery@gmail.com>
2026-05-11 09:22:04 +0200

    style: drop in := req.Msg alias; access fields via req.Msg.GetX()
    Per user preference: Connect handler bodies pull each field directly
    from req.Msg, no local alias. Keeps the call site self-documenting.

--- rpc.go
+++ rpc.go
@@ -31,32 +31,31 @@ func (r *rpc) loadProcesses(name string) ([]*Process, error) {
 }
 
 func (r *rpc) CreateService(_ context.Context, req *connect.Request[serviceV1.Create]) (*connect.Response[serviceV1.Response], error) {
-	in := req.Msg
-	r.p.logger.Debug("create service", "name", in.GetName(), "restart_sec", in.GetRestartSec(), "command", in.GetCommand(), "process number", in.GetProcessNum())
+	r.p.logger.Debug("create service", "name", req.Msg.GetName(), "restart_sec", req.Msg.GetRestartSec(), "command", req.Msg.GetCommand(), "process number", req.Msg.GetProcessNum())
 
-	if in.GetProcessNum() == 0 {
-		return nil, connect.NewError(connect.CodeInvalidArgument, fmt.Errorf("the service with %s name should have at least 1 process", in.GetName()))
+	if req.Msg.GetProcessNum() == 0 {
+		return nil, connect.NewError(connect.CodeInvalidArgument, fmt.Errorf("the service with %s name should have at least 1 process", req.Msg.GetName()))
 	}
 
 	r.mu.Lock()
 	defer r.mu.Unlock()
 
-	if _, ok := r.p.processes.Load(in.GetName()); ok {
-		return nil, connect.NewError(connect.CodeAlreadyExists, fmt.Errorf("%w: %s", errServiceExists, in.GetName()))
+	if _, ok := r.p.processes.Load(req.Msg.GetName()); ok {
+		return nil, connect.NewError(connect.CodeAlreadyExists, fmt.Errorf("%w: %s", errServiceExists, req.Msg.GetName()))
 	}
 
-	procs := make([]*Process, 0, in.GetProcessNum())
-	for range int(in.GetProcessNum()) {
+	procs := make([]*Process, 0, req.Msg.GetProcessNum())
+	for range int(req.Msg.GetProcessNum()) {
 		proc := NewServiceProcess(&Service{
-			Command:         in.GetCommand(),
-			ProcessNum:      int(in.GetProcessNum()),
-			ExecTimeout:     time.Second * time.Duration(in.GetExecTimeout()),
-			RemainAfterExit: in.GetRemainAfterExit(),
-			RestartSec:      in.GetRestartSec(),
-			UseServiceName:  in.GetServiceNameInLogs(),
-			TimeoutStopSec:  in.GetTimeoutStopSec(),
-			Env:             in.GetEnv(),
-		}, in.GetName(), r.p.logger)
+			Command:         req.Msg.GetCommand(),
+			ProcessNum:      int(req.Msg.GetProcessNum()),
+			ExecTimeout:     time.Second * time.Duration(req.Msg.GetExecTimeout()),
+			RemainAfterExit: req.Msg.GetRemainAfterExit(),
+			RestartSec:      req.Msg.GetRestartSec(),
+			UseServiceName:  req.Msg.GetServiceNameInLogs(),
+			TimeoutStopSec:  req.Msg.GetTimeoutStopSec(),
+			Env:             req.Msg.GetEnv(),
+		}, req.Msg.GetName(), r.p.logger)
 
 		if err := proc.start(); err != nil {
 			// if some process from the group failed -> deallocate the whole group
@@ -72,20 +71,19 @@ func (r *rpc) CreateService(_ context.Context, req *connect.Request[serviceV1.Cr
 		procs = append(procs, proc)
 	}
 
-	r.p.processes.Store(in.GetName(), procs)
+	r.p.processes.Store(req.Msg.GetName(), procs)
 	return connect.NewResponse(&serviceV1.Response{Ok: true}), nil
 }
 
 func (r *rpc) Terminate(_ context.Context, req *connect.Request[serviceV1.Service]) (*connect.Response[serviceV1.Response], error) {
-	in := req.Msg
-	r.p.logger.Debug("terminate service", "name", in.GetName())
+	r.p.logger.Debug("terminate service", "name", req.Msg.GetName())
 
 	r.mu.Lock()
 	defer r.mu.Unlock()
 
-	v, ok := r.p.processes.LoadAndDelete(in.GetName())
+	v, ok := r.p.processes.LoadAndDelete(req.Msg.GetName())
 	if !ok {
-		return nil, connect.NewError(connect.CodeNotFound, fmt.Errorf("%w: %s", errNoSuchService, in.GetName()))
+		return nil, connect.NewError(connect.CodeNotFound, fmt.Errorf("%w: %s", errNoSuchService, req.Msg.GetName()))
 	}
 	for _, proc := range v.([]*Process) {
 		proc.stop()
@@ -95,13 +93,12 @@ func (r *rpc) Terminate(_ context.Context, req *connect.Request[serviceV1.Servic
 }
 
 func (r *rpc) Restart(_ context.Context, req *connect.Request[serviceV1.Service]) (*connect.Response[serviceV1.Response], error) {
-	in := req.Msg
-	r.p.logger.Debug("restart service", "name", in.GetName())
+	r.p.logger.Debug("restart service", "name", req.Msg.GetName())
 
 	r.mu.Lock()
 	defer r.mu.Unlock()
 
-	procs, err := r.loadProcesses(in.GetName())
+	procs, err := r.loadProcesses(req.Msg.GetName())
 	if err != nil {
 		return nil, err
 	}
@@ -113,29 +110,28 @@ func (r *rpc) Restart(_ context.Context, req *connect.Request[serviceV1.Service]
 		svc := &Service{}
 		*svc = *(procs[i]).service
 
-		newProc := NewServiceProcess(svc, in.GetName(), r.p.logger)
+		newProc := NewServiceProcess(svc, req.Msg.GetName(), r.p.logger)
 		if err := newProc.start(); err != nil {
-			r.p.processes.Delete(in.GetName())
+			r.p.processes.Delete(req.Msg.GetName())
 			return nil, connect.NewError(connect.CodeInternal, err)
 		}
 
 		newProcs[i] = newProc
-		r.p.processes.Delete(in.GetName())
+		r.p.processes.Delete(req.Msg.GetName())
 	}
 
-	r.p.processes.Store(in.GetName(), newProcs)
+	r.p.processes.Store(req.Msg.GetName(), newProcs)
 	return connect.NewResponse(&serviceV1.Response{Ok: true}), nil
 }
 
 // Deprecated: use GetStatuses to get correct info.
 func (r *rpc) GetStatus(_ context.Context, req *connect.Request[serviceV1.Service]) (*connect.Response[serviceV1.Status], error) {
-	in := req.Msg
-	r.p.logger.Debug("service status", "name", in.GetName())
+	r.p.logger.Debug("service status", "name", req.Msg.GetName())
 
 	r.mu.RLock()
 	defer r.mu.RUnlock()
 
-	procs, err := r.loadProcesses(in.GetName())
+	procs, err := r.loadProcesses(req.Msg.GetName())
 	if err != nil {
 		return nil, err
 	}
@@ -157,13 +153,12 @@ func (r *rpc) GetStatus(_ context.Context, req *connect.Request[serviceV1.Servic
 }
 
 func (r *rpc) GetStatuses(_ context.Context, req *connect.Request[serviceV1.Service]) (*connect.Response[serviceV1.Statuses], error) {
-	in := req.Msg
-	r.p.logger.Debug("service status", "name", in.GetName())
+	r.p.logger.Debug("service status", "name", req.Msg.GetName())
 
 	r.mu.RLock()
 	defer r.mu.RUnlock()
 
-	procs, err := r.loadProcesses(in.GetName())
+	procs, err := r.loadProcesses(req.Msg.GetName())
 	if err != nil {
 		return nil, err
 	}
